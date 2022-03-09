5e919b5ce7d7a3c512a3a5522f0c8695ac2a40be
Hugook11 <100297000+Hugook11@users.noreply.github.com>
2022-03-09 18:14:20 +0100

    chat

--- pages/formulaire.vue
+++ pages/formulaire.vue
@@ -1,19 +1,19 @@
 <template>
-<div>
-  <form @submit.prevent="postData">
-    <div class="mb-3">
-      <label for="inputPseudo" class="form-label">Pseudo</label>
-      <input type="text" class="form-control" v-model="pseudo" />
-    </div>
-    <div class="mb-3">
-      <label for="exampleInputPassword1" class="form-label">Message</label>
-      <textarea type="textarea" class="form-control" v-model="message" />
-    </div>
-    <button type="submit" class="btn btn-primary">Envoyer</button>
-  </form>
   <div>
+    <form @submit.prevent="postData">
+      <div class="mb-3">
+        <label for="inputPseudo" class="form-label">Pseudo</label>
+        <input type="text" class="form-control" v-model="pseudo" />
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Message</label>
+        <textarea type="textarea" class="form-control" v-model="message" />
+      </div>
+      <button type="submit" class="btn btn-primary">Envoyer</button>
+    </form>
+    <br>
 
-  </div>
+    <div id="messages" class=""></div>
   </div>
 </template>
 
@@ -22,30 +22,58 @@ export default {
   name: "Formulaire",
   layout: "default",
 
-  data(){
-    return{
-      pseudo:"",
-      message:"",
-      indexMessages: 1,
-    }
+  data() {
+    return {
+      pseudo: "",
+      message: "",
+    };
   },
 
-  methods:{
-    postData(){
-      try{
-      this.$fire.firestore.collection("Messages").doc().set({
-        Pseudo:this.pseudo,
-        Message:this.message
-      })
-      }catch(err){
-        console.log(err)
+  mounted:function() {
+          this.getMessages();
+        },
+
+  methods: {
+    postData() {
+      try {
+        this.$fire.firestore.collection("Messages").doc().set({
+          Pseudo: this.pseudo,
+          Message: this.message,
+          Date: new Date().getTime(),
+        });
+      } catch (err) {
+        console.log(err);
       }
-      this.indexMessages++
-    }
-  }
+    },
+
+    getMessages() {
+      this.$fire.firestore
+        .collection("Messages")
+        .orderBy("Date", "desc")
+        .get()
+        .then((snap) => {
+          snap.forEach((doc) => {
+            this.$el.querySelector("#messages").innerHTML += `
+            <div class="card" style="width: 60%">
+              <div class="card-body">
+                <h5 class="card-title">${doc.data().Pseudo}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${
+                  doc.data().Date
+                }</h6>
+                <p class="card-text">${doc.data().Message}</p>
+              </div>
+            </div><br>`;
+          });
+        });
+    },
+
+
+  
+  
+  },
+
 };
 </script>
 
 <style scoped>
-
 </style>
